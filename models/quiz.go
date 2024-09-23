a78b2415d8143d54220db4ec03686bf2b4405994
ayomenjadi <ayomenjadi@gmail.com>
2024-09-23 02:54:21 +0000

    ci(Dockerfile) : Add dockerfile

--- models/quiz.go
+++ models/quiz.go
@@ -4,19 +4,21 @@ import "gorm.io/gorm"
 
 type Quiz struct {
 	gorm.Model
-	Qustion string `json:"qustion"`
-	Qustionurl string `json:"qustionurl"`
-	Answurl string `json:"answurl"`
-	Answ string `json:"answer"`
-	Optiona   string `json:"optiona"`
-	Optionb   string `json:"optionb"`
-	Optionc   string `json:"optionc"`
-	Optiond   string `json:"optiond"`
-	Optione   string `json:"optione"`
-	Optionurla   string `json:"optionurla"`
-	Optionurlb   string `json:"optionurlb"`
-	Optionurlc   string `json:"optionurlc"`
-	Optionurld   string `json:"optionurld"`
-	Optionurle   string `json:"optionurle"`
-	Tipe   int    `json:"tipe"`
+	Question    string `json:"question"`
+	Questionurl string `json:"questionurl"`
+	Answurl     string `json:"answurl"`
+	Answ        string `json:"answer"`
+	Optiona     string `json:"optiona"`
+	Optionb     string `json:"optionb"`
+	Optionc     string `json:"optionc"`
+	Optiond     string `json:"optiond"`
+	Optione     string `json:"optione"`
+	Optionurla  string `json:"optionurla"`
+	Optionurlb  string `json:"optionurlb"`
+	Optionurlc  string `json:"optionurlc"`
+	Optionurld  string `json:"optionurld"`
+	Optionurle  string `json:"optionurle"`
+	Result      string `json:"result"`
+	Tipe        string `json:"tipe"`
+	Val         bool   `json:"value"`
 }
